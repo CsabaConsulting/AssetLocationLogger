f42d678a67f176f507cc856b95532d7b3841fd89
MrCsabaToth <csaba.toth.us@gmail.com>
2020-05-23 18:18:37 -0700

    Remove unused imports

--- app/src/main/java/dev/csaba/diygpsmanager/viewmodel/MapsViewModel.kt
+++ app/src/main/java/dev/csaba/diygpsmanager/viewmodel/MapsViewModel.kt
@@ -3,11 +3,9 @@ package dev.csaba.diygpsmanager.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import dev.csaba.diygpsmanager.addTo
 import dev.csaba.diygpsmanager.data.FirestoreReportRepository
 import dev.csaba.diygpsmanager.data.IReportRepository
